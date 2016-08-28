308d60ef4ecd68b640a3cf3bf7c18393ba1b11cd
Will McAuliff <womcauliff@gmail.com>
2016-08-27 23:04:09 -0400

    main() function, checking for minimum arguments necessary

--- liri.js
+++ liri.js
@@ -1,1 +1,24 @@
 var importedKeys = require('./keys.js');
+// Status code for success is 0
+var status = main();
+console.log(status);
+
+/**
+ * main():
+ * Outermost function called at program start.
+ *
+ * @return {number} 0 for success, 1 on error
+ */
+function main() {
+	console.log("main()");
+	//Checking if argument length is less than the 
+	//minimum number of arguments for any command.
+	if (process.argv.length < 3) {
+		console.log("Error: invalid number of arguments."
+			+ "\nUsage: node liri.js [command] [arg]");
+		return(1);
+	}
+	else {
+		return (0);
+	}
+}
